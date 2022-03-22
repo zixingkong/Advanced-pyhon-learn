f07e505e22597803f032a06c176b46622f024c56
harveyywang <harveyywang@tencent.com>
2022-03-22 19:39:42 +0800

    add: 学习yield from的含义及其使用方法和示例

---
+++ Learn/yield_learn/yield_from_example.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     yield_from_example
+   Description :   学习 yield from
+   date：          2022/3/22
+-------------------------------------------------
+"""
+
+final_result = {}
+
+
+def sum(pro_name):
+    total = 0
+    num = []
+    while True:
+        x = yield
+        print(pro_name + '销量：', x)
+        if not x:
+            break
+        total += x
+        num.append(x)
+    return total, num
+
+
+def middle(key):
+    while True:
+        final_result[key] = yield from sum(key)
+        print(key + '统计完成 ！！！')
+
+
+def main():
+    data_sets = {
+        "bobby牌面膜": [1200, 1500, 3000],
+        "bobby牌手机": [28, 55, 98, 108],
+        "bobby牌大衣": [280, 560, 778, 70],
+    }
+
+    for key, values in data_sets.items():
+        print("start key:", key)
+        mid = middle(key)
+        mid.send(None)
+        for value in values:
+            mid.send(value)
+        mid.send(None)
+
+    print('final result:', final_result)
+
+
+if __name__ == '__main__':
+    main()
--- chapter_12_coroutine_and_async/03.yield_from_test.py
+++ chapter_12_coroutine_and_async/03.yield_from_test.py
@@ -7,13 +7,14 @@
 # @Description: 
 -------------------------------------------------
 """
+
 # python3.3新加了yield from语法
 from itertools import chain
 
 my_list = [1, 2, 3]
 my_dict = {
-	"bobby1": "http://projectsedu.com",
-	"bobby2": "http://www.imooc.com",
+    "bobby1": "http://projectsedu.com",
+    "bobby2": "http://www.imooc.com",
 }
 
 
@@ -34,23 +35,25 @@ my_dict = {
 
 
 def my_chain(*args, **kwargs):
-	for my_iterable in args:
-		yield from my_iterable
-	# for value in my_iterable:
-	#     yield value
+    for my_iterable in args:
+        yield from my_iterable
+
+
+# for value in my_iterable:
+#     yield value
 
 
 for value in my_chain(my_list, my_dict, range(5, 10)):
-	print(value)
+    print(value)
 
 
 def g1(gen):
-	yield from gen
+    yield from gen
 
 
 def main():
-	g = g1()
-	g.send(None)
+    g = g1()
+    g.send(None)
 
 # 1. main 调用方 g1(委托生成器) gen 子生成器
 # 1. yield from会在调用方与子生成器之间建立一个双向通道
